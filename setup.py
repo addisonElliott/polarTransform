cf61efd56d29d0fd0bf96ebe9599dba921b92284
Addison Elliott <addison.elliott@gmail.com>
2018-10-28 17:15:04 -0500

    Fix mistake in setuptools configuration

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='polarTransform',
           'Development Status :: 4 - Beta',
           'Topic :: Scientific/Engineering',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3'
+          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6'
